26eef8ea47bca6559aa5b1a1ae04f4b72d1478b4
omaralhafni <omarh-1997@hotmail.com>
2023-08-06 09:55:00 +0300

    fix: debounce

--- app/utils/debounce.ts
+++ app/utils/debounce.ts
@@ -1,8 +1,10 @@
-export const debounce = (func: (...args: any[]) => void, delay: number) => {
+export const debounce = <T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+) => {
   let timeoutId: ReturnType<typeof setTimeout>;
-  return function (...args: any[]) {
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => func.apply(this, args), delay);
   };
 };
-
